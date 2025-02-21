7ca776eb09cd1c84e7f8cc92982c9ba920d5985c
Tiffany P <tiffany__pham@outlook.com>
2025-02-20 18:23:07 -0700

    Preliminary ex5

---
+++ ex5.py
@@ -0,0 +1,33 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+def binary_search(arr, val, start, end):
+    if start == end:
+        if arr[start] > val:
+            return start
+        else:
+            return start+1
+    
+    if start > end:
+        return start
+    
+    mid = (start + end) // 2
+    if arr[mid] < val:
+        return binary_search(arr, val, mid+1, end)
+    elif arr[mid] > val:
+        return binary_search(arr, val, start, mid-1)
+    else:
+        return mid
+
+def binary_insertion_sort(arr):
+    for i in range(1, len(arr)):
+        val = arr[i]
+        j = binary_search(arr, val, 0, i-1)
+        arr = arr[:j] + [val] + arr[j:i] + arr[i+1:]
+    return arr
